ca8d9609843dde41ac54360b99519b3c7adb9683
Shaun Manda <shaunwmanda@gmail.com>
2025-09-06 13:30:16 +0300

    add new alx-react-app-new and update Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,6 +1,9 @@
 const Footer = () => {
   return (
     <footer>
+      <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+        <h1>City Lovers</h1>
+      </header>
       <p>© 2023 City Lovers</p>
     </footer>
   );
