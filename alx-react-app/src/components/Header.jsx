6203573e26dedbafd4d0eaad37ad530411a16ddc
Shaun Manda <shaunwmanda@gmail.com>
2025-09-06 13:42:09 +0300

    add style too Header

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,9 +1,8 @@
 const Header = () => {
   return (
-     <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
       <h1>My Favorite Cities</h1>
     </header>
-
 
   );
 };
