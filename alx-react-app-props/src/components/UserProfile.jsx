599d6c786823a4e38e249516df333dffa38ceb76
Shaun Manda <shaunwmanda@gmail.com>
2025-09-07 08:46:14 +0300

    add usercontext to UserProfile component

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import UserContext from "./UserContext";
 
 function UserProfile() {
   const [name, setName] = useState("John Doe");
   const [age, setAge] = useState(30);
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      <p>Name: {name}</p>
-      <p>Age: {age}</p>
-    </div>
+    <UserContext.Provider value={{ name, age }}>
+      <div>
+        <h1>User Profile</h1>
+        <p>Name: {name}</p>
+        <p>Age: {age}</p>
+      </div>
+    </UserContext.Provider>
   );
 }
 
