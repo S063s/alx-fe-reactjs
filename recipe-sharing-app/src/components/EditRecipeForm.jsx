1a31fddd67371a1fb49a87016b45402e874eca75
Shaun Manda <shaunwmanda@gmail.com>
2025-09-13 12:31:36 +0300

    add import useState from 'react' in EditRecipeForm

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react';
+
 const EditRecipeForm = ({ recipe }) => {
   const updateRecipe = useRecipeStore(state => state.updateRecipe);
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     updateRecipe({ ...recipe, title, description });
   };
 
