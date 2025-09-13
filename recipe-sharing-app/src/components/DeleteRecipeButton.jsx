a1095b98720f1c6bbde13d8270c65413f5f3099d
Shaun Manda <shaunwmanda@gmail.com>
2025-09-13 12:35:26 +0300

    add usenavigate from react-router-dom to delete btn

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,12 @@
 import { useRecipeStore } from './recipeStore';
+import useNavigate from 'react-router-dom';
 
 const RecipeDetails = ({ recipeId }) => {
   const recipe = useRecipeStore(state =>
     state.recipes.find(recipe => recipe.id === recipeId)
   );
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+  const navigate = useNavigate();
 
   return (
     <div>
