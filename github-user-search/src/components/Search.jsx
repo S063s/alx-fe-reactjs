be4fadf74d57d6b155c0d857c282f9e0cefc7bf9
Shaun Manda <shaunwmanda@gmail.com>
2025-09-20 15:13:03 +0300

    modified search to look more clean and organized

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -18,16 +18,39 @@ const Search = ({ onSearch, loading, error }) => {
           handleSearch();
         }}
         placeholder="Search GitHub users..."
-        avatar={<img src="/path/to/avatar/image.png" alt="Avatar" />}
-        login={<img src="/path/to/login/image.png" alt="Login" />}
-        error={<img src="/path/to/error/image.png" alt="Error" />}
+        avatar_url={
+          <img
+            src="/path/to/avatar/image.png"
+            alt="GitHub user avatar, neutral expression, set against a plain background"
+          />
+        }
+        login={
+          <img
+            src="/path/to/login/image.png"
+            alt="Login icon, stylized person silhouette, displayed on a simple interface"
+          />
+        }
+        error={
+          <img
+            src="/path/to/error/image.png"
+            alt="Error icon, red exclamation mark inside a circle, conveys warning or alert"
+          />
+        }
         onChange={(e) => setQuery(e.target.value)}
-        form='search-form'
+        form="search-form"
       />
 
       <button onClick={handleSearch}>Search</button>
       {loading && <p>Loading...</p>}
-      {error && <p style={{ color: 'red' }}>Looks like we can't find the user, <img src="/path/to/error/image.png" alt="Error" /></p>}
+      {error && (
+        <p style={{ color: 'red' }}>
+          Looks like we can't find the user,{' '}
+          <img
+            src="/path/to/error/image.png"
+            alt="Error icon, red exclamation mark inside a circle, conveys warning or alert"
+          />
+        </p>
+      )}
     </div>
   );
 };
