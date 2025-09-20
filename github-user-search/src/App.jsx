fcee21b1d76cf237707d2b57c68d8af1a7ea666a
Shaun Manda <shaunwmanda@gmail.com>
2025-09-20 12:58:25 +0300

    add import statements for components and services

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import components from './components'
+import services from './services'
 
 function App() {
   const [count, setCount] = useState(0)
