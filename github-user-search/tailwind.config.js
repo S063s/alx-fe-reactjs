540ecdc00dc65e50beaf062843557cc053590ac1
Shaun Manda <shaunwmanda@gmail.com>
2025-09-20 16:20:09 +0300

    edit tailwind config

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./src/index.css",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+
+  ],
 }
 
