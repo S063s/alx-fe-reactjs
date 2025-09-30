feb0ecc54428c666bc5dff7c621fcd8cac15f138
Shaun Manda <shaunwmanda@gmail.com>
2025-09-30 21:47:27 +0300

    add new HomePage component to implement landing page and show recipes

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+function HomePage() {
+  const [Recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    // Fetch recipes from the API or use a static JSON file
+    const fetchRecipes = async () => {
+      const response = await fetch("src/data.json");
+      const data = await response.json();
+      setRecipes(data);
+    };
+
+    fetchRecipes();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold md:text-4xl">Recipe Sharing Platform</h1>
+      {/* Render recipes */}
+      <div className="container mx-auto p-4">
+        {Recipes.map((recipe, ) => (
+          <div key={index} className="mb-8">
+            <img className="w-full h-64 object-cover mb-4" src={recipe.image} alt={recipe.title} />
+            <h2 className="text-xl font-semibold">{recipe.title}</h2>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default HomePage;
