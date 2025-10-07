559699cef3580f032985a0a66953b4a913c54d05
Shaun Manda <shaunwmanda@gmail.com>
2025-10-07 16:42:08 +0300

    .

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,20 +1,52 @@
 import { useState } from 'react';
-import FormikForm from './formikForm';
+import { Formik } from 'formik';
 
 const RegistrationForm = () => {
   return (
-    <FormikForm>
-      <label htmlFor="username">Username:</label>
-      <input type="text" id="username" name="username" required />
+    <Formik
+      initialValues={{ value: { username: '', email: '', password: '' } }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+            />
 
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" required />
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+            />
 
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" name="password" required />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+            />
 
-      <button type="submit">Register</button>
-    </FormikForm>
+        <button type="submit">Register</button>
+          </div>
+        </form>
+      )}
+    </Formik>
   );
 };
 
