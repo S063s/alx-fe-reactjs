7037f807aaad7f3dd045563aa39a571bf38a27ca
Shaun Manda <shaunwmanda@gmail.com>
2025-10-07 17:02:35 +0300

    add usestate in RegistrationForm component

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,53 +1,38 @@
 import { useState } from 'react';
-import { Formik } from 'formik';
 
-const RegistrationForm = () => {
-  return (
-    <Formik
-      initialValues={{ value: { username: '', email: '', password: '' } }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ handleChange, handleBlur, handleSubmit }) => (
+function RegistrationForm() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
+
+
+    const validate = () => {
+        const newErrors = {};
+        if (!username) newErrors.username = 'Username is required';
+        if (!email) newErrors.email = 'Email is required';
+        if (!password) newErrors.password = 'Password is required';
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (validate()) {
+            console.log('Form submitted:', { username, email, password });
+        }
+    };
+
+    return (
         <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              required
-            />
-
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              required
-            />
-
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              required
-            />
-
-        <button type="submit">Register</button>
-          </div>
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            {errors.username && <span>{errors.username}</span>}
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            {errors.email && <span>{errors.email}</span>}
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            {errors.password && <span>{errors.password}</span>}
+            <button type="submit">Register</button>
         </form>
-      )}
-    </Formik>
-  );
-};
+    );
+}
 
 export default RegistrationForm;
