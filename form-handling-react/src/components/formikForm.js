b5273f61cfc05c996e9bf2235ada0399ef94fbf1
Shaun Manda <shaunwmanda@gmail.com>
2025-10-07 17:21:22 +0300

    implement use of Formik library for form handling in React

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,45 +1,47 @@
-const FormikForm = () => {
-  return (
-    <Formik
-      initialValues={{ username: "", email: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ handleChange, handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            onChange={handleChange}
-            required
-          />
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            onChange={handleChange}
-            required
-          />
+const validationSchema = Yup.object({
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Invalid email format").required("Email is required"),
+  password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+});
 
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            onChange={handleChange}
-            required
-          />
+function FormikForm() {
+    const initialValues = { username: "", email: "", password: "" };
+    function handleSubmit(values, { setSubmitting, resetForm }) {
+        console.log("Form submitted:", values);
+        setSubmitting(false);
+        resetForm();
+    }
 
-          <button type="submit">Register</button>
-        </form>
-      )}
-    </Formik>
-  );
-};
+    return (
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+            {({ isSubmitting }) => (
+                <Form>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <Field type="text" id="username" name="username" />
+                        <ErrorMessage name="username" component="div" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <Field type="email" id="email" name="email" />
+                        <ErrorMessage name="email" component="div" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <Field type="password" id="password" name="password" />
+                        <ErrorMessage name="password" component="div" />
+                    </div>
+                    <button type="submit" disabled={isSubmitting}>
+                        Submit
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    );
+}
 
 export default FormikForm;
