a8321b64c2bdbb91f720015a8cf3c1a997e634e6
Shaun Manda <shaunwmanda@gmail.com>
2025-10-07 17:56:18 +0300

    .

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,7 +1,8 @@
-import usequery from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
+
 
 function PostsComponent() {
-  const { isLoading, error, data } = usequery({
+  const { isLoading, isError, fetchData } = useQuery({
     queryKey: ['posts'],
     queryFn: () =>
       fetch('https://jsonplaceholder.typicode.com/posts').then((res) =>
@@ -10,7 +11,7 @@ function PostsComponent() {
   })
 
     if (isLoading) return 'Loading...'
-    if (error) return 'Error fetching posts'
+    if (isError) return 'Error fetching posts'
     return (
       <ul>
         {data.map((post) => (
